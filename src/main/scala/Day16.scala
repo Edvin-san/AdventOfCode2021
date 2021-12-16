d9283c0509cedf4752fa56d7457abeed23fc9dc0
Edvin Lundberg <edvin_lundberg@msn.com>
2021-12-16 23:40:42 +0100

    day16 prettify

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -2,14 +2,10 @@ import DayCase.{Puzzle, Test}
 import Input.{InputString, ResourceInput}
 import zio._
 
-import scala.util.{Random, Try}
+import scala.util.Try
 
 object Day16 extends Day[Long, BigInt] {
-  val doDebug = false
-  def debug(s: String) = if (doDebug) println(s)
-
   case class VersionNumber(value: Int) extends AnyVal
-
   case class TypeId(value: Int) extends AnyVal
 
   object TypeId {
@@ -22,118 +18,76 @@ object Day16 extends Day[Long, BigInt] {
 
   object Packet {
     case class Literal(version: VersionNumber, number: BigInt) extends Packet
-
     case class Operator(version: VersionNumber, typeId: TypeId, operands: List[Packet]) extends Packet
   }
 
-  trait Parser[+A] {
-    self =>
+  trait Parser[+A] { self =>
     def parse(s: String): Option[(A, String)]
 
-    def flatMap[B](f: A => Parser[B]): Parser[B] = new Parser[B] {
-      override def parse(s: String): Option[(B, String)] = self.parse(s) match {
-        case Some((a, remainder)) =>
-          f(a).parse(remainder)
-        case None => None
-      }
+    def flatMap[B](f: A => Parser[B]): Parser[B] = (s: String) => self.parse(s) match {
+      case Some((a, remainder)) =>
+        f(a).parse(remainder)
+      case None => None
     }
 
     def map[B](f: A => B): Parser[B] = flatMap(a => Parser.succeed(f(a)))
 
-    def withFilter(f: A => Boolean): Parser[A] = new Parser[A] {
-      override def parse(s: String): Option[(A, String)] = self.parse(s) match {
-        case Some((a, remainder)) => Option.when(f(a))(a -> remainder)
-        case None => None
-      }
+    def withFilter(f: A => Boolean): Parser[A] = (s: String) => self.parse(s) match {
+      case Some((a, remainder)) => Option.when(f(a))(a -> remainder)
+      case None => None
     }
 
-    def feedInto[B](p: Parser[B])(implicit ev: A <:< String): Parser[B] = new Parser[B] {
-      override def parse(s: String): Option[(B, String)] = self.parse(s).flatMap {
-        case (a, remainder) => p.parse(a).map { case (b, _) => b -> remainder }
-      }
+    def feedInto[B](p: Parser[B])(implicit ev: A <:< String): Parser[B] = (s: String) => self.parse(s).flatMap {
+      case (a, remainder) => p.parse(a).map { case (b, _) => b -> remainder }
     }
+
+    def *>[B](other: Parser[B]): Parser[B] = flatMap(_ => other)
   }
 
   object Parser {
-    def succeed[A](a: A): Parser[A] = new Parser[A] {
-      override def parse(s: String): Option[(A, String)] = Some(a -> s)
-    }
+    def succeed[A](a: A): Parser[A] = (s: String) => Some(a -> s)
 
-    def multiple[A](min: Int, max: Option[Int])(p: Parser[A]): Parser[List[A]] = new Parser[List[A]] {
-      override def parse(input: String): Option[(List[A], String)] = {
-        val id = s"[multiple${Random.nextInt(100)}]"
-        debug(s"$id Will try to parse between $min and $max")
-        val successfulParseResults = LazyList
-          .iterate(p.parse(input))(_.flatMap { case (_, rest) => p.parse(rest) })
-          .zipWithIndex
-          .takeWhile { case (res, idx) => res.isDefined && max.forall(_ > idx) }
-          .tapEach(r => debug(s"$id parsed $r"))
-          .map(_._1.get)
-          .toList
-        debug(s"$id managed to parse ${successfulParseResults.size}, given min = $min and max = $max")
-        debug(s"$id full result of parse ${successfulParseResults.map(_._1)}")
-        Option.when(successfulParseResults.size >= min) {
-          val lastRemainder = successfulParseResults.lastOption.map(_._2).getOrElse(input)
-          successfulParseResults.map(_._1) -> lastRemainder
-        }
+    def multiple[A](min: Int, max: Option[Int])(p: Parser[A]): Parser[List[A]] = (input: String) => {
+      val successfulParseResults = LazyList
+        .iterate(p.parse(input))(_.flatMap { case (_, rest) => p.parse(rest) })
+        .zipWithIndex
+        .takeWhile { case (res, idx) => res.isDefined && max.forall(_ > idx) }
+        .map(_._1.get)
+        .toList
+      Option.when(successfulParseResults.size >= min) {
+        val lastRemainder = successfulParseResults.lastOption.map(_._2).getOrElse(input)
+        successfulParseResults.map(_._1) -> lastRemainder
       }
     }
 
-    def parseChar(c: Char): Parser[Char] = new Parser[Char] {
-      override def parse(s: String): Option[(Char, String)] = s.headOption.filter(_ == c).map(_ -> s.tail)
-    }
+    def parseChar(c: Char): Parser[Char] = (s: String) => s.headOption.filter(_ == c).map(_ -> s.tail)
+    def parseString(len: Int): Parser[String] = (s: String) =>
+      if (s.size >= len) Some(s.take(len) -> s.drop(len))
+      else None
 
-    def parseString(len: Int): Parser[String] = new Parser[String] {
-      override def parse(s: String): Option[(String, String)] =
-        if (s.size >= len) Some(s.take(len) -> s.drop(len))
-        else None
-    }
-
-    val parseInt: Parser[Int] = new Parser[Int] {
-      override def parse(s: String): Option[(Int, String)] = Try(Integer.parseInt(s, 2)).toOption.map(i => i -> "")
-    }
-
-    val parseBigInt: Parser[BigInt] = new Parser[BigInt] {
-      override def parse(s: String): Option[(BigInt, String)] = Try(BigInt(s, 2)).toOption.map(i => i -> "")
-    }
+    val parseInt: Parser[Int] = (s: String) => Try(Integer.parseInt(s, 2)).toOption.map(i => i -> "")
+    val parseBigInt: Parser[BigInt] = (s: String) => Try(BigInt(s, 2)).toOption.map(i => i -> "")
 
     def parseIntOfLen(len: Int): Parser[Int] = parseString(len).feedInto(parseInt)
 
-    val eatZeroes: Parser[Unit] = (s: String) => Some(() -> s.dropWhile(_ == '0'))
     val versionNumberParser: Parser[VersionNumber] = parseIntOfLen(3).map(VersionNumber.apply)
     val typeIdParser: Parser[TypeId] = parseIntOfLen(3).map(TypeId.apply)
 
-    val literalByte1Parser: Parser[String] = for {
-      _ <- parseChar('1')
-      s <- parseString(4)
-    } yield s
-
-    val literalByteFinalParser: Parser[String] = for {
-      _ <- parseChar('0')
-      s <- parseString(4)
-    } yield s
-
     def literalPacketParser(versionNumber: VersionNumber): Parser[Packet.Literal] = for {
-      _ <- Parser.succeed(debug(s"Beginning to parse a literal $versionNumber"))
-      bytes <- multiple(0, None)(literalByte1Parser)
-      lastByte <- literalByteFinalParser
+      bytes <- multiple(0, None)(parseChar('1') *> parseString(4))
+      lastByte <- parseChar('0') *> parseString(4)
       number <- Parser.succeed(bytes.mkString + lastByte).feedInto(parseBigInt)
     } yield Packet.Literal(versionNumber, number)
 
     def operatorPacketParser(version: VersionNumber, typeId: TypeId): Parser[Packet.Operator] = for {
-      _ <- Parser.succeed(debug(s"Beginning to parse an operator $version, $typeId"))
-      i <- parseIntOfLen(1)
-      subpackets <- i match {
+      lengthTypeId <- parseIntOfLen(1)
+      subpackets <- lengthTypeId match {
         case 0 => for {
-          _ <- Parser.succeed(debug(s"I was 0, so will try to parse bitLen subpackets"))
           bitLen <- parseIntOfLen(15)
-          _ = debug(s"bitLen = $bitLen")
           sps <- parseString(bitLen).feedInto(multiple(min = 1, max = None)(packetParser))
         } yield sps
         case 1 => for {
-          _ <- Parser.succeed(debug(s"I was 1, so will try to parse numSubpackets subpackets"))
           numSubpackets <- parseIntOfLen(11)
-          _ = debug(s"numSubpackets = $numSubpackets")
           sps <- multiple(min = numSubpackets, max = Some(numSubpackets))(packetParser)
         } yield sps
       }
@@ -141,15 +95,12 @@ object Day16 extends Day[Long, BigInt] {
 
     val packetParser: Parser[Packet] = for {
       version <- versionNumberParser
-      _ = debug(s"Parsed $version")
       typeId <- typeIdParser
-      _ = debug(s"Parsed $typeId")
       packet <-
         if (typeId == TypeId.LiteralTypeId) literalPacketParser(version)
         else operatorPacketParser(version, typeId)
     } yield packet
   }
-
 
   def hexToBinary(hexString: String): String = {
     val unpadded = BigInt(hexString, 16).toString(2)
